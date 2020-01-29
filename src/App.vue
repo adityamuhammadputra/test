be10b2dfeb6ff8a67fca6986b4b60dad2ebcead4
adit <adityamuhamamdputra@gmail.com>
2020-01-29 16:48:45 +0700

    fisrt

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="#fcb69f"
+      dark
+      shrink-on-scroll
+      src="https://picsum.photos/1920/1080?random"
+      scroll-target="#scrolling-techniques-2"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+      <route-link to="/" class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </route-link>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        <route-link to="/about">About</route-link>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+     <v-sheet
+      id="scrolling-techniques-2"
+      class="overflow-y-auto"
+      max-height="720"
+    >
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+    </v-sheet>
+  </v-app>
+</template>
+
+<script>
+</script>
