8d7b3e88bff404de86aca09e19446098d36fd42c
adit <adityamuhamamdputra@gmail.com>
2020-02-04 16:45:20 +0700

    add banner and about

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     src="https://picsum.photos/1920/1080?random">
     <v-container >
       <v-row>
-        <v-col cols="6" class="home-banner-left">
+        <v-col cols="6" class="home-banner-left animated fadeInLeft">
           <h1>Hello</h1>
           <h4>I'm <b>Adit's </b>
             <b>
@@ -24,11 +24,22 @@
           <p>
             Lorem Ipsum is <b>simply dummy </b>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
           </p>
+          <v-btn class="ma-2" tile outlined color="secondary">
+            <v-icon left>mdi-phone</v-icon> Phone
+          </v-btn>
+          <v-btn class="ma-2" tile outlined color="secondary">
+            <v-icon left>mdi-email</v-icon> Mail
+          </v-btn>
         </v-col>
-        <v-col cols="6">
-        b
+        <v-col cols="6" class="animated fadeInRight">
+          <v-img src="../assets/img/ban.png"/>
         </v-col>
       </v-row>
+    </v-container>
+
+    <v-container fluid style="padding-right:0px;padding-left:0px;">
+      <v-img
+        src="../assets/img/C.png"/>
     </v-container>
   </div>
 </template>
